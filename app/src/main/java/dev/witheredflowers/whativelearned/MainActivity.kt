324a03f808bcbfec398e20dd8c520964491eb1c2
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-04-04 22:18:16 +0700

    feat: adding change locale fab and icon for fab (text EN and ID)

--- app/src/main/java/dev/witheredflowers/whativelearned/MainActivity.kt
+++ app/src/main/java/dev/witheredflowers/whativelearned/MainActivity.kt
@@ -2,10 +2,20 @@ package dev.witheredflowers.whativelearned
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fabChangeLanguage: FloatingActionButton = findViewById(R.id.fabChangeLocale)
+
+        fabChangeLanguage.setOnClickListener {
+            // TODO: Change this to add locale logic
+            // source: https://lokalise.com/blog/kotlin-internationalization/
+            Toast.makeText(applicationContext, "Halo", Toast.LENGTH_SHORT).show()
+        }
     }
 }
