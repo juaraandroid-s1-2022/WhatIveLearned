99297a3fc9d2cfa45446049b51c8bde4d296896e
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-04-07 11:34:39 +0700

    feat: adding content for Project Lemonade (Unit 1 - Done)

--- app/src/main/java/dev/witheredflowers/whativelearned/Unit1.kt
+++ app/src/main/java/dev/witheredflowers/whativelearned/Unit1.kt
@@ -1,11 +1,20 @@
 package dev.witheredflowers.whativelearned
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import dev.witheredflowers.whativelearned.lemonade.MainActivity
 
 class Unit1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_unit1)
+        
+        val btnLemonade:Button = findViewById(R.id.btnLemonade)
+        btnLemonade.setOnClickListener() {
+            val intent = Intent(this@Unit1, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
