c6856521a58cc2b03295b2c608f67dc352067c80
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-04-09 09:36:31 +0700

    feat: adding content Apps: Lunch Tray - Navigation Graph

--- app/src/main/java/dev/witheredflowers/whativelearned/Unit3.kt
+++ app/src/main/java/dev/witheredflowers/whativelearned/Unit3.kt
@@ -1,11 +1,24 @@
 package dev.witheredflowers.whativelearned
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.witheredflowers.whativelearned.databinding.ActivityUnit3Binding
+import dev.witheredflowers.whativelearned.lunchtray.MainActivity
 
 class Unit3 : AppCompatActivity() {
+	private lateinit var binding: ActivityUnit3Binding
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_unit3)
+		
+		binding = ActivityUnit3Binding.inflate(layoutInflater)
+		
+		setContentView(binding.root)
+		
+		binding.btnLunchTray.setOnClickListener {
+			val intent = Intent(this@Unit3, MainActivity::class.java)
+			startActivity(intent)
+		}
 	}
 }
